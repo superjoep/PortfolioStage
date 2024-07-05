779566f62aaece3221bc7ffa50f95c7d1142543c
“superjoep” <joepdekock@gmail.com>
2024-07-05 16:40:13 +0200

    Skills view updated

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,9 +1,41 @@
 <template>
      <div  class="rounded-3xl hover:shadow-xl  p-8 bg-green-400 shadow-md text-white col-span-2 sm:col-span-1 md:col-span-1 h-auto md:h-full flex flex-col"> 
         <div style="font-family: Mainheader;" class="text-xl text-center">Skills </div>
-        <div style="font-family: maintext;" class="pt-5">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet finibus turpis, placerat commodo nulla interdum non. Quisque blandit ante lorem, eget tincidunt justo consectetur vel. Aliquam a arcu est. Aliquam commodo turpis at risus sagittis, sed interdum libero laoreet. Suspendisse lectus nulla, semper nec magna vel, posuere consequat massa. Quisque risus erat, congue ac sapien a, fringilla tempus diam. Sed elementum enim metus, et hendrerit ex consequat vitae. Aenean posuere justo sed accumsan ultricies. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec tincidunt erat sit amet enim feugiat molestie.
-            
+        <div style="font-family: maintext;" class="pt-5 grid grid-cols-2 place-content-center">
+        <list>
+          <div  style="font-weight: bolder; font-size: large; font-family: maintext;">Programming</div>
+          <div class="pt-5" style="font-weight: bolder; font-size: large; font-family: maintext;">Advanced</div>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Tailwind</li>
+          <li>Bootstrap</li>
+          <div class="pt-5" style="font-weight: bolder; font-size: large; font-family: maintext;">Proficient</div>
+          <li>Javascript</li>
+          <li>Vue</li>
+          <li>SQL</li>
+          <div class="pt-5" style="font-weight: bolder; font-size: large; font-family: maintext;">Basics</div>
+          <li>PHP</li>
+          <li>C#</li>
+          <li>React</li>
+          <li>Python</li>
+        </list>     
+        <list>
+          <div  style="font-weight: bolder; font-size: large; font-family: maintext;">Other skills</div>
+          <div class="pt-5" style="font-weight: bolder; font-size: large; font-family: maintext;">Advanced</div>
+          <li>Wordpress</li>
+          <li>Word</li>
+          <li>Photoshop</li>
+          <li>Photoshop</li>
+          <div class="pt-5" style="font-weight: bolder; font-size: large; font-family: maintext;">Proficient</div>
+          <li>Illustrator</li>
+          <li>Indesign</li>
+          <li>Excel</li>
+          <li>Unity</li>
+          <div class="pt-5" style="font-weight: bolder; font-size: large; font-family: maintext;">Basics</div>
+          <li>Machine teaching</li>
+          <li>Data collection</li>
+          
+        </list>       
         </div>
 
     </div>
@@ -11,6 +43,8 @@
 
   
   <script>
+import { list } from 'postcss';
+
   export default {
     name: 'Skills'
   }
---
+++ src/views/contactView.vue
@@ -0,0 +1,15 @@
+<script>
+import contact from '../components/Contact.vue';
+export default {
+  name: 'App',
+  components: {
+    contact
+  }
+}
+</script>
+<template>
+    <div class="flex justify-center flex-row gap-4 flex-wrap"> 
+    <contact />
+  </div>
+</template>
+
