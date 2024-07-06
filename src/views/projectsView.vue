3518154d50f821d5fe6fa2accb5df9d36e681f13
“superjoep” <joepdekock@gmail.com>
2024-07-06 11:56:59 +0200

    project pages made dynamic

--- src/views/projectsView.vue
+++ src/views/projectsView.vue
@@ -1,15 +1,18 @@
 <script>
 import projects from '../components/myproj.vue';
+import indivProject from '../components/Projects/oneProjectPage.vue';
 export default {
   name: 'App',
   components: {
-    projects
+    projects,
+    indivProject
   }
 }
 </script>
 <template>
     <div class="flex justify-center flex-row gap-4 flex-wrap"> 
     <projects />
+    <indivProject />
   </div>
 </template>
 
