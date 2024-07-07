b48a140d815116fbdeacf1aef95a2779f9229616
“superjoep” <joepdekock@gmail.com>
2024-07-08 01:03:52 +0200

    animated text

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,22 +1,57 @@
 <template>
-    <div class="rounded-3xl p-8 bg-white hover:shadow-xl text-black shadow-md col-span-2 basis-1/2 sm:col-span-1 md:col-span-1 h-auto md:h-full flex "> 
-        <div style="font-family: maintext;">
-          <img class="float-start size-40  mr-4" src="../assets/images/Memoji.jpeg">
-          <div style="font-family: Mainheader; display:inline;" class="text-2xl">Hi there!</div> I’m Joep, a 23-year-old ICT student from Eindhoven. For the past seven years, I’ve been on an exciting journey through the world of technology, exploring everything from front-end and back-end development to AI, VR, and AR.
+  <div class="rounded-3xl p-8 bg-white hover:shadow-xl text-black shadow-md col-span-2 basis-1/2 sm:col-span-1 md:col-span-1 h-auto md:h-full flex">
+    <div style="font-family: maintext;">
+      <img class="float-start size-40 mr-4" src="../assets/images/Memoji.jpeg" />
+      <div style="font-family: Mainheader; display:inline;" class="text-2xl hi-there-container">
+        <span v-for="(char, index) in greeting" :key="index" :class="{ 'hi-there-char': char !== ' ', 'space': char === ' ' }">
+          {{ char }}
+        </span>
+      </div>
+      I’m Joep, a 23-year-old ICT student from Eindhoven. For the past seven years, I’ve been on an exciting journey through the world of technology, exploring everything from front-end and back-end development to AI, VR, and AR.
 
-        My tech adventure started at Summa College with a focus on Application Media Development, and now I’m pursuing my bachelor’s in IT at Fontys University. Along the way, I’ve had several internships that have not only expanded my knowledge but also provided me with hands-on experience in these fascinating fields.
+      My tech adventure started at Summa College with a focus on Application Media Development, and now I’m pursuing my bachelor’s in IT at Fontys University. Along the way, I’ve had several internships that have not only expanded my knowledge but also provided me with hands-on experience in these fascinating fields.
 
-        I’m excited about the future and eager to continue learning and growing in the world of ICT!
-        </div>
-
+      I’m excited about the future and eager to continue learning and growing in the world of ICT!
     </div>
+  </div>
+</template>
 
-  </template>
-
-  
-  <script>
-  export default {
-    name: 'About'
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      greeting: 'Hi there!'
+    };
   }
-  </script>
-  
+};
+</script>
+
+<style>
+.hi-there-container {
+  display: inline-block;
+}
+
+.hi-there-char {
+  display: inline-block;
+  animation: upDown 0.5s ease-in-out infinite alternate;
+}
+
+.space {
+  margin-right: 1; /* Adjust the margin to increase space */
+}
+
+.hi-there-char:nth-child(1) { animation-delay: 0s; }
+.hi-there-char:nth-child(2) { animation-delay: 0.1s; }
+.hi-there-char:nth-child(3) { animation-delay: 0.2s; }
+.hi-there-char:nth-child(4) { animation-delay: 0.3s; }
+.hi-there-char:nth-child(5) { animation-delay: 0.4s; }
+.hi-there-char:nth-child(6) { animation-delay: 0.5s; }
+.hi-there-char:nth-child(7) { animation-delay: 0.6s; }
+.hi-there-char:nth-child(8) { animation-delay: 0.7s; }
+
+@keyframes upDown {
+  0% { transform: translateY(0); }
+  100% { transform: translateY(-5px); }
+}
+</style>
